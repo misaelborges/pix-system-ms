4ded54fb7100f93da127a68edfd830e73484e058
Misael Borges <mizaelborges2011@gmail.com>
2026-01-23 09:52:54 -0300

    feat: create pix-key-service project structure    - Projeto Spring Boot    - Dependências no pom.xml    - application.properties

---
+++ pix-key-service/src/main/java/com/financeiro/pixkey/PixKeyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.pixkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixKeyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixKeyServiceApplication.class, args);
+	}
+
+}
