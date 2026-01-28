b1be6bc7418c0e64df3694ee4693ffc17fdae24b
Misael Borges <mizaelborges2011@gmail.com>
2026-01-28 11:33:45 -0300

    feat: create receipt-service with PDF generation    - Projeto Spring Boot    - Dependências (web, jpa, amqp, itext7)    - application.properties    - Compila

---
+++ receipt-service/src/test/java/com/financeiro/receipt/ReceiptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
